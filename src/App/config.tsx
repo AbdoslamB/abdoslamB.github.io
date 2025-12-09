426a76fa1ca226c079810617911c4a837f9b2617
Abdoslam Baabbad <43893502+AbdoslamB@users.noreply.github.com>
2025-12-09 00:46:38 -0500

    Update config.tsx

--- src/App/config.tsx
+++ src/App/config.tsx
@@ -3,10 +3,10 @@ import { Config } from 'types';
 
 export const config: Config = {
   name: {
-    display: 'Adam Alston',
+    display: 'Abdoslam Baabbad',
   },
   title: {
-    display: 'Software Engineer',
+    display: '',
   },
   buttons: [
     {
@@ -14,28 +14,28 @@ export const config: Config = {
       display: 'GitHub',
       ariaLabel: 'GitHub profile (opens in new window)',
       icon: <GitHub />,
-      href: 'https://github.com/adamalston/',
+      href: 'https://github.com/AbdoslamB/',
     },
     {
       name: 'linked-in',
       display: 'LinkedIn',
       ariaLabel: 'LinkedIn profile (opens in new window)',
       icon: <LinkedIn />,
-      href: 'https://www.linkedin.com/in/adam-alston/',
+      href: 'https://www.linkedin.com/in/abdoslambaabbad',
     },
     {
       name: 'resume',
       display: 'Resume',
       ariaLabel: 'Resume in Google Drive (opens in new window)',
       icon: <Resume />,
-      href: 'https://drive.google.com/file/d/1VQ_Oeim_e92QEMi64ejGWY5Hf4RRxfeJ/view',
+      href: '',
     },
     {
       name: 'email',
       display: 'Email',
       ariaLabel: 'Email contact (opens in new window)',
       icon: <Email />,
-      href: 'mailto:aalston9@gmail.com',
+      href: 'mailto:abdoslam.baabbad+github@gmail.com',
     },
   ],
 };
